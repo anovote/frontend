5a579425aa68c7c8ef7ad2c261c0a3dcf86f8759
Steffen Holanger <holanger.steffen@gmail.com>
2021-01-25 14:02:58 +0100

    Add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import './App.css'
 
-function App() {
-    return (
-        <div className="App">
-        </div>
-    )
+function App(): React.ReactElement {
+    return <div className="App"></div>
 }
 
 export default App
