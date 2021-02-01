39a549d44781967c4667023c830cbb65723c72df
Christoffer A Traeen <christoffer.andersen@live.com>
2021-02-01 11:34:14 +0100

    Migrate to scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'antd/dist/antd.min.css'
-import './style/app.min.css'
+import './style/scss/main.scss'
 
 function App(): React.ReactElement {
     return <div className="App"></div>
