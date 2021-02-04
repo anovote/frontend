60d392d9db60005b0002d222072ce29f3811b129
Christoffer A Traeen <christoffer.andersen@live.com>
2021-02-04 12:30:19 +0100

    Add login view test

---
+++ src/views/login/Loginview.test.tsx
@@ -0,0 +1,45 @@
+// LoginView.test.js
+
+import React from 'react'
+import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import { mockMatchMedia } from '../../core/testMock/mockMatchMedia'
+
+import LoginView from './index'
+
+let container: Element | DocumentFragment
+
+beforeAll(() => {
+    mockMatchMedia()
+})
+beforeEach(() => {
+    container = document.createElement('div')
+    document.body.appendChild(container)
+})
+
+afterEach(() => {
+    unmountComponentAtNode(container)
+})
+
+it('displays forms validation on empty fields after submit is clicked', async () => {
+    await act(async () => {
+        render(<LoginView />, container)
+        container.querySelector('button')?.click()
+        return await new Promise((resolve) => {
+            setTimeout(() => {
+                resolve()
+            }, 200)
+        })
+    })
+    const list = [...container.querySelectorAll('.ant-form-item-explain')]
+    expect(list[0]).toBeTruthy()
+    expect(list[1]).toBeTruthy()
+})
+
+it('does not displays forms validation message before clicking the submit button', async () => {
+    await act(async () => {
+        render(<LoginView />, container)
+    })
+    const list = [...container.querySelectorAll('.ant-form-item-explain')]
+    expect(list.length).toBe(0)
+})
