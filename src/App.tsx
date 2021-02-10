7e725694773f23c457009d837f2a392a31a5a4d9
Emil <emil.nilsen9888@gmail.com>
2021-02-10 14:29:55 +0100

    Add register form

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
 import 'antd/dist/antd.min.css'
 import './style/scss/main.scss'
-
 import RegisterView from './views/register'
-
 function App(): React.ReactElement {
     return (
-        <Router>
-            <div className="App">
-                <RegisterView />
-            </div>
-        </Router>
+        <div className="App">
+            <RegisterView />
+        </div>
     )
 }
 
--- src/views/register/index.tsx
+++ src/views/register/index.tsx
@@ -1,12 +1,52 @@
-import Layout from 'antd/lib/layout/layout'
+import { Button, Form, Input } from 'antd'
+import Layout, { Content } from 'antd/lib/layout/layout'
 import * as React from 'react'
-import { Content } from 'antd/lib/layout/layout'
 
 export default function RegisterView(): React.ReactElement {
     return (
         <Layout className="layout">
-            <Content className="is-fullscreen is-flex-colum has-content-center-center">
+            <Content className="is-fullscreen is-flex-column has-content-center-center">
                 <h1>ANOVOTE</h1>
+                <div className="register-form">
+                    <Form className="is-flex-column" layout="vertical" name="register-form">
+                        <Form.Item
+                            label="Fornavn"
+                            name="firstName"
+                            rules={[{ required: true, message: 'Husk fornavn!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            label="Etternavn"
+                            name="lastName"
+                            rules={[{ required: true, message: 'Husk etternavn!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="Epost" name="email" rules={[{ required: true, message: 'Husk epost!' }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[{ required: true, message: 'Husk passord!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+                        <Form.Item
+                            label="Skriv passord igjen"
+                            name="reTypePassword"
+                            rules={[{ required: true, message: 'Husk passord på nytt!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit">
+                                Registrer
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
             </Content>
         </Layout>
     )
