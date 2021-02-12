4beb02e538848f32d3090c62b6d3c4c7e824f584
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-12 14:51:02 +0100

    add logic

--- src/views/changePassword/ChangePassword.tsx
+++ src/views/changePassword/ChangePassword.tsx
@@ -1,4 +1,4 @@
-import { Form, Input, Button, Divider } from 'antd'
+import { Form, Input, Button, Divider, Alert } from 'antd'
 import Layout, { Content } from 'antd/lib/layout/layout'
 import React from 'react'
 
@@ -8,24 +8,41 @@ export default function ChangePassword(): React.ReactElement {
         //throw new NotImplementedException()
     }
 
-    const validateForm = async (form: any) => {
-        console.log(form)
+    const [errorMessage, setErrorMessage] = React.useState('')
+    const [errorDescription, setErrorDescription] = React.useState('')
+    const mediumRegex = new RegExp(
+        '^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{8,})', // todo add comment
+    )
+
+    // typescript-eslint: disable no-explicit-any
+    /* typescript-eslint-disable  no-explicit-any */
+    const validateForm = async (values: ChangePasswordInterface) => {
+        const { password1, password2 } = values
+        if (password1 !== password2) {
+            setErrorMessage('Passwords does not match')
+            setErrorDescription('Please try again')
+        }
     }
 
     return (
         <Layout className="layout">
             <Content className="is-fullscreen is-flex-column has-content-center-center">
                 <h1>Change you password</h1>
+                <div className="error-field">
+                    {!!errorMessage && (
+                        <Alert message={errorMessage} description={errorDescription} type={'error'} showIcon closable />
+                    )}
+                </div>
                 <Form onAbort={onabort} onFinish={validateForm}>
                     <Form.Item
                         label="New password"
                         name="password1"
-                        rules={[{ required: true, message: 'Please fill out' }]}
+                        rules={[{ required: true, message: 'Please fill out', min: 8, pattern: mediumRegex }]}
                     >
                         <Input.Password />
                     </Form.Item>
                     <Form.Item
-                        label="Retype password"
+                        label="Re-type password"
                         name="password2"
                         rules={[{ required: true, message: 'Please fill out' }]}
                     >
@@ -46,3 +63,7 @@ export default function ChangePassword(): React.ReactElement {
     )
 }
 // todo if password1 is visible don't show password2
+interface ChangePasswordInterface {
+    password1: string
+    password2: string
+}
