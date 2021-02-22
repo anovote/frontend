e47a7fe94c2328eda873f646195111df3f91ed99
Emil <emil.nilsen9888@gmail.com>
2021-02-22 15:46:19 +0100

    Add switch styling

---
+++ src/components/switch/SwitchComponent.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { Switch } from 'antd'
+
+export default function SwitchComponent(): React.ReactElement {
+    return <Switch className="switch-component" />
+}
