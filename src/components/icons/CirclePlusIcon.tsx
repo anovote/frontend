8902b342bb299731e5e819cb8c8cf0920a9cf422
Christoffer A Traeen <christoffer.andersen@live.com>
2021-02-23 14:01:02 +0100

    Create large buttons

---
+++ src/components/icons/CirclePlusIcon.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from 'react'
+export const CirclePlussIcon = (): ReactElement => {
+    return (
+        <div className="circle-center-content main-contrasting">
+            <div className="pluss"></div>
+        </div>
+    )
+}
