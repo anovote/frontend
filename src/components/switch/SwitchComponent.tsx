4b906816f43aa1328faad010acc4aa88498aa32f
Emil <emil.nilsen9888@gmail.com>
2021-02-23 07:57:19 +0100

    Update structure

--- src/components/switch/SwitchComponent.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-import { Switch } from 'antd'
-
-export default function SwitchComponent(): React.ReactElement {
-    return <Switch />
-}
--- src/components/switch/SwitchWithLabel.tsx
+++ src/components/switch/SwitchWithLabel.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react'
-import SwitchComponent from './SwitchComponent'
 import Text from 'antd/lib/typography/Text'
-import { Space } from 'antd'
+import { Space, Switch } from 'antd'
 
 export default function SwitchWithLabel({ label }: { label: string }): React.ReactElement {
     return (
         <span>
             <Space size="large">
                 <Text>{label}</Text>
-                <SwitchComponent />
+                <Switch />
             </Space>
         </span>
     )
