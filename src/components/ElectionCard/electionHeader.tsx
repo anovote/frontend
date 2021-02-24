d534d7377c663ee4c5e375f531c3e2f650ac0513
Sander Hurlen <sander.hurlen@icloud.com>
2021-02-24 19:13:34 +0100

    refactor header to display correct info

--- src/components/ElectionCard/electionHeader.tsx
+++ src/components/ElectionCard/electionHeader.tsx
@@ -1,22 +1,40 @@
+import { PlusOutlined } from '@ant-design/icons'
 import { Button, Col, Row, Space } from 'antd'
 import Title from 'antd/lib/typography/Title'
+import { ElectionStatus } from 'core/models/IElectionStatus'
 import React, { ReactElement } from 'react'
-import { PlusOutlined } from '@ant-design/icons'
 
-export default function ElectionHeader({ title, count }: { title: string; count: number }): ReactElement {
+export default function ElectionHeader({
+    type,
+    title,
+    count,
+}: {
+    type: ElectionStatus
+    title: string
+    count: number | undefined
+}): ReactElement {
+    let info = (
+        <Space className="col-position" align="center">
+            <Button id="card-add-btn" type="primary" icon={<PlusOutlined />} size="small" />
+            <span className="card-total-count">{count}</span>
+        </Space>
+    )
+    if (type == ElectionStatus.Finished) {
+        info = (
+            <Space className="col-position" align="center">
+                <span className="card-total-count">{count}</span>
+            </Space>
+        )
+    }
+
     return (
-        <>
-            <Row justify="space-between" align="top">
-                <Col span={12}>
-                    <Title level={3}>{title}</Title>
-                </Col>
-                <Col span={6} id="header-info">
-                    <Space className="col-position" align="center">
-                        <Button id="card-add-btn" type="primary" icon={<PlusOutlined />} size="small" />
-                        <span className="card-total-count">{count}</span>
-                    </Space>
-                </Col>
-            </Row>
-        </>
+        <Row justify="space-between" align="top">
+            <Col span={12}>
+                <Title level={3}>{title}</Title>
+            </Col>
+            <Col span={6} id="header-info">
+                {info}
+            </Col>
+        </Row>
     )
 }
