932a41bae4e0d885e4337934176bd6d191d9f21d
Christoffer A Traeen <christoffer.andersen@live.com>
2021-02-24 21:52:59 +0100

    Add open close handlers from skeleton

--- src/containers/modal/ProfileSettingsModal.tsx
+++ src/containers/modal/ProfileSettingsModal.tsx
@@ -3,35 +3,26 @@ import Button from 'antd/lib/button/button'
 import Modal from 'antd/lib/modal/Modal'
 import Title from 'antd/lib/typography/Title'
 import ChangePasswordForm from 'containers/forms/profile/ChangePasswordForm'
-import React, { useState } from 'react'
+import React, { ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
 
-export default function ProfileSettingsModal() {
-    const [isModalVisible, setIsModalVisible] = useState(false)
+export default function ProfileSettingsModal({
+    showModal,
+    close,
+}: {
+    showModal: boolean
+    close: () => void
+}): ReactElement {
     const [t] = useTranslation(['translation', 'common', 'form', 'profile'])
 
-    const showModal = () => {
-        setIsModalVisible(true)
-    }
-
-    const handleCancel = () => {
-        setIsModalVisible(false)
-    }
-
-    function one() {
-        console.log('hei')
-    }
     return (
         <>
-            <Button type="primary" onClick={showModal}>
-                Open Modal
-            </Button>
             <Modal
                 width={'100vw'}
                 title={t('profile:Update profile')}
                 footer={null}
-                visible={isModalVisible}
-                onCancel={handleCancel}
+                visible={showModal}
+                onCancel={close}
                 className="modal-display-small"
             >
                 <Row>
