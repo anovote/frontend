a9f7199db62d554494c487322f62f4d0bee30e66
Sander Hurlen <sander.hurlen@icloud.com>
2021-02-24 19:14:07 +0100

    refactor electionCard to have more props

--- src/components/ElectionCard/index.tsx
+++ src/components/ElectionCard/index.tsx
@@ -1,28 +1,24 @@
 import { Card, List } from 'antd'
+import { IElection } from 'core/models/IElection'
+import { ElectionStatus } from 'core/models/IElectionStatus'
 import React, { ReactElement } from 'react'
 import ElectionEntry from './electionEntry'
 import ElectionHeader from './electionHeader'
 interface ElectionCardProps {
-    type: string
+    type: ElectionStatus
+    title: string
+    data?: IElection[]
 }
-export default function ElectionCard({ type }: ElectionCardProps): ReactElement {
-    const data = [
-        'Racing car sprays burning fuel into crowd.',
-        'Japanese princess to wed commoner.',
-        'Australian walks 100km after outback crash.',
-        'Man charged over missing wedding girl.',
-        'Los Angeles battles huge wildfires.',
-    ]
+export default function ElectionCard({ type, title, data }: ElectionCardProps): ReactElement {
     return (
         <Card className="election-card">
             <List
                 className="election-card-list"
-                header={<ElectionHeader title={type} count={data.length} />}
-                bordered
+                header={<ElectionHeader type={type} title={title} count={data ? data.length : 0} />}
                 dataSource={data}
-                renderItem={(item) => (
+                renderItem={(election) => (
                     <List.Item>
-                        <ElectionEntry title={item} date="1.1.2020"></ElectionEntry>
+                        <ElectionEntry election={election}></ElectionEntry>
                     </List.Item>
                 )}
             />
