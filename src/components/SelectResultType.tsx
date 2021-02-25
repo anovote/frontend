62459fb9c5b37f1a172a43862c46375de6820e85
Emil <emil.nilsen9888@gmail.com>
2021-02-25 10:05:31 +0100

    Add localization

--- src/components/SelectResultType.tsx
+++ src/components/SelectResultType.tsx
@@ -4,22 +4,25 @@ import { ReactComponent as NoneIcon } from '../style/assets/none.svg'
 import { ReactComponent as SingleWinnerIcon } from '../style/assets/single.svg'
 import { ReactComponent as RankedIcon } from '../style/assets/ranked.svg'
 import Text from 'antd/lib/typography/Text'
+import { useTranslation } from 'react-i18next'
 
 export default function SelectResultType(): React.ReactElement {
+    const [t] = useTranslation(['common'])
+
     return (
         <div className="radio-wrapper">
             <Radio.Group className="radio-group">
                 <Radio value={1} className="radio-button">
                     <NoneIcon className="none-icon" />
-                    <Text className="radio-text select-none-text">None</Text>
+                    <Text className="radio-text select-none-text">{t('None')}</Text>
                 </Radio>
                 <Radio value={2} className="radio-button">
                     <SingleWinnerIcon className="single-icon" />
-                    <Text className="radio-text select-single-text">Single winner</Text>
+                    <Text className="radio-text select-single-text">{t('Single winner')}</Text>
                 </Radio>
                 <Radio value={3} className="radio-button">
                     <RankedIcon className="ranked-icon" />
-                    <Text className="radio-text select-ranked-text">Ranked</Text>
+                    <Text className="radio-text select-ranked-text">{t('Ranked')}</Text>
                 </Radio>
             </Radio.Group>
         </div>
