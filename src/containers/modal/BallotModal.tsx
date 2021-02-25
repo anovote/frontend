681c4e372e027acb5646047f8591295714611bbb
Christoffer A Traeen <christoffer.andersen@live.com>
2021-02-25 21:20:51 +0100

    Add ballot modal

--- src/containers/modal/BallotModal.tsx
+++ src/containers/modal/BallotModal.tsx
@@ -1,65 +1,76 @@
+import { LeftOutlined, RightOutlined } from '@ant-design/icons'
 import { Col, Row } from 'antd'
 import Modal from 'antd/lib/modal/Modal'
 import Title from 'antd/lib/typography/Title'
-import ChangeEmailFrom from 'containers/forms/profile/ChangeEmailForm'
-import ChangePasswordForm from 'containers/forms/profile/ChangePasswordForm'
+import StatCard from 'components/statCard/StatCard'
+import { IControl } from 'core/helpers/IControl'
+import { BallotEntity } from 'core/models/ballot/BallotEntity'
+import { BallotVoteStats } from 'core/models/ballot/BallotVoteStats'
 import React, { ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
-interface IControl {
-    previous: () => void
-    next: () => void
-}
-
-interface IBallotStats {
-    total: number
-    voter: number
-    blank: number
-}
-
-interface ICandidate {
-    id: number
-    title: string
-    votes: number
-}
-interface ICandidateStats {
-    candidates: Array<ICandidate>
-}
 
 export default function BallotModal({
     showModal,
     close,
-    initalData,
+    controls,
+    ballotStats,
+    ballotEntity,
 }: {
     showModal: boolean
     close: () => void
+    controls: IControl
+    ballotStats: BallotVoteStats
+    ballotEntity: BallotEntity
 }): ReactElement {
-    const [t] = useTranslation(['translation', 'common', 'form', 'profile'])
+    const [t] = useTranslation(['translation', 'common'])
+    const { previous, next } = controls
+    const { total, votes, blank, candidates } = ballotStats
+    const footer = (
+        <div className="spread">
+            <button onClick={previous} className="inline-icon text-label button-no-style">
+                <LeftOutlined />
+                <div>{t('common:Previous')}</div>
+            </button>
+            <button onClick={next} className="inline-icon text-label button-no-style">
+                <div>{t('common:Next')}</div>
+                <RightOutlined />
+            </button>
+        </div>
+    )
+
+    const voteStats = [
+        { title: t('common:Total'), value: total },
+        { title: t('common:Vote_plural'), value: votes },
+        { title: t('common:Blank'), value: blank },
+    ]
 
     return (
         <>
             <Modal
                 width={'100vw'}
-                title={'Ballot'}
-                footer={null}
+                // TODO Add status ICON implementation here when merged
+                title={ballotEntity.status}
+                footer={footer}
                 visible={showModal}
                 onCancel={close}
                 className="modal-display-small"
             >
                 <Row>
                     <Col span={24}>
-                        <Title level={2}>Name of person</Title>
+                        <Title level={2}>{ballotEntity.title}</Title>
+                        <div>
+                            <StatCard stats={voteStats} inverseColors={true} />
+                        </div>
                     </Col>
-                </Row>
-                <Row>
                     <Col span={24}>
-                        <Title level={3}>{t('profile:Change email')}</Title>
-                        <ChangeEmailFrom />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={24}>
-                        <Title level={3}>{t('profile:Change password')}</Title>
-                        <ChangePasswordForm />
+                        {/*This mapping is for representation til we have a graph library */}
+                        {candidates.map((candidate, index) => {
+                            return (
+                                <div key={index}>
+                                    Name: {candidate.candidate} Votes: {candidate.votes}
+                                </div>
+                            )
+                        })}
                     </Col>
                 </Row>
             </Modal>
