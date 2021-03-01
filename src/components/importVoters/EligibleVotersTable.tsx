1959461c2b8e14db030f2a3885c16f6922a53fad
Emil <emil.nilsen9888@gmail.com>
2021-03-01 11:52:25 +0100

    Add uploading of file

--- src/components/importVoters/EligibleVotersTable.tsx
+++ src/components/importVoters/EligibleVotersTable.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
-import { Table } from 'antd'
-import ImportEligibleVotersDropdown from 'components/election/ImportEligibleVotersDropdown'
+import { Table, Upload, Button, UploadProps } from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+import { RcFile } from 'antd/lib/upload'
 
 export default function EligibleVotersTable(): React.ReactElement {
     const columns = [
@@ -11,9 +12,19 @@ export default function EligibleVotersTable(): React.ReactElement {
         },
     ]
 
+    const [fileList, setFileList] = React.useState<RcFile[]>([])
+
+    const saveFile = (file: RcFile): boolean => {
+        setFileList([...fileList, file])
+        console.log(fileList)
+        return false
+    }
+
     return (
         <div className="voters-table-container">
-            <ImportEligibleVotersDropdown />
+            <Upload beforeUpload={saveFile}>
+                <Button icon={<PlusOutlined />} size="large" shape="circle"></Button>
+            </Upload>
             <Table columns={columns} />
         </div>
     )
