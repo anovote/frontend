eacb5465ad347f8300cdcba26e759c75e521f8e8
Sander Hurlen <sander.hurlen@icloud.com>
2021-03-01 11:10:55 +0100

    add ballot queue

--- src/components/queue/BallotsQueue.tsx
+++ src/components/queue/BallotsQueue.tsx
@@ -1,7 +1,10 @@
 import { Divider, Steps } from 'antd'
+import Title from 'antd/lib/typography/Title'
+import PushBallotIcon from 'components/icons/PushBallotIcon'
+import StatCard from 'components/statCard/StatCard'
+import SquareIconButton from 'containers/button/SquareIconButton'
 import React, { ReactElement, useState } from 'react'
 import QueueDescription from './QueueDescription'
-import QueueHeader from './QueueHeader'
 
 const { Step } = Steps
 
@@ -10,19 +13,47 @@ export default function BallotsQueue({ dataSource }: { dataSource: any[] }): Rea
 
     const queue = []
 
+    const timeline = []
+
+    const b = [
+        { title: 'Total', value: 99999 },
+        { title: 'Votes', value: 3343 },
+        { title: 'Votes', value: 94 },
+    ]
+
     for (const ballot of dataSource) {
-        queue.push(<Step key={ballot.id} title={<QueueHeader />} description={<QueueDescription />} />)
+        queue.push(
+            <Step
+                key={ballot.id}
+                title={<Title level={4}>{ballot.title}</Title>}
+                subTitle={<QueueDescription />}
+                description={
+                    <>
+                        <StatCard stats={b} />{' '}
+                        <SquareIconButton
+                            text="Push Ballot"
+                            tabIndex={0}
+                            classId="push-ballot-button"
+                            onClick={() => console.log('log ballot id: ', ballot.id)}
+                        >
+                            <PushBallotIcon />
+                        </SquareIconButton>
+                    </>
+                }
+            />,
+        )
+        timeline.push(<Step key={ballot.id} />)
     }
 
     return (
         <div className="ballots-queue">
             <Steps current={current} progressDot={true}>
-                {queue}
+                {timeline}
             </Steps>
 
             <Divider />
 
-            <Steps current={current} onChange={setCurrent} direction="vertical">
+            <Steps current={current} onChange={setCurrent} direction="vertical" className="queue">
                 {queue}
             </Steps>
         </div>
