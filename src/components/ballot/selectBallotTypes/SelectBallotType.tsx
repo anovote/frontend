10264e69a5cb0922a4cab7a1a48949db8fb08704
Christoffer A Traeen <christoffer.andersen@live.com>
2021-03-01 12:22:49 +0100

    Add form item to select ballot

--- src/components/ballot/selectBallotTypes/SelectBallotType.tsx
+++ src/components/ballot/selectBallotTypes/SelectBallotType.tsx
@@ -1,4 +1,4 @@
-import { Radio } from 'antd'
+import { Radio, Form } from 'antd'
 import Text from 'antd/lib/typography/Text'
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
@@ -6,24 +6,27 @@ import SelectMultipleIcon from './SelectMultipleIcon'
 import SelectOneIcon from './SelectOneIcon'
 import SelectRankedIcon from './SelectRankedIcon'
 
+export default function SelectBallotType({ label }: { label: string }): React.ReactElement {
     const [t] = useTranslation(['ballot'])
 
     return (
-        <div className="radio-wrapper">
-            <Radio.Group className="radio-group">
-                <Radio value={1} className="radio-button">
-                    <SelectMultipleIcon />
+        <Form.Item label={label} name={'ballot-type'}>
+            <div className="radio-wrapper">
+                <Radio.Group className="radio-group">
+                    <Radio value={1} className="radio-button">
+                        <SelectMultipleIcon />
                         <Text className="radio-text select-type-text">{t('ballot:Select multiple')}</Text>
-                </Radio>
-                <Radio value={2} className="radio-button">
-                    <SelectOneIcon />
+                    </Radio>
+                    <Radio value={2} className="radio-button">
+                        <SelectOneIcon />
                         <Text className="radio-text select-type-text">{t('ballot:Select one')}</Text>
-                </Radio>
-                <Radio value={3} className="radio-button">
-                    <SelectRankedIcon />
+                    </Radio>
+                    <Radio value={3} className="radio-button">
+                        <SelectRankedIcon />
                         <Text className="radio-text select-type-text">{t('ballot:Select ranked')}</Text>
-                </Radio>
-            </Radio.Group>
-        </div>
+                    </Radio>
+                </Radio.Group>
+            </div>
+        </Form.Item>
     )
 }
