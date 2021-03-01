891a1d34d4f8a63850150ffea8f1739e58387494
Sander Hurlen <sander.hurlen@icloud.com>
2021-03-01 16:36:17 +0100

    add translation message

--- src/components/queue/BallotsQueue.tsx
+++ src/components/queue/BallotsQueue.tsx
@@ -1,23 +1,25 @@
-import { Divider, Steps } from 'antd'
+import { Steps } from 'antd'
 import Title from 'antd/lib/typography/Title'
 import PushBallotIcon from 'components/icons/PushBallotIcon'
 import StatCard from 'components/statCard/StatCard'
 import SquareIconButton from 'containers/button/SquareIconButton'
 import { BallotEntity } from 'core/models/ballot/BallotEntity'
 import React, { ReactElement, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import QueueDescription from './QueueDescription'
 
 const { Step } = Steps
 
 export default function BallotsQueue({ dataSource }: { dataSource: BallotEntity[] }): ReactElement {
     const [current, setCurrent] = useState(0)
+    const [t] = useTranslation(['common'])
 
     const queue = []
 
     const stats = [
-        { title: 'Total', value: 143 },
-        { title: 'Issues', value: 23 },
-        { title: 'Votes', value: 94 },
+        { title: t('common:Total'), value: 143 },
+        { title: t('common:Issues'), value: 23 },
+        { title: t('Votes'), value: 94 },
     ]
 
     function pushBallot(id: number) {
