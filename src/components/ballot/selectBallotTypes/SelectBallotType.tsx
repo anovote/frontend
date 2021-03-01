f3c07c910d3fbb680f58d205f36c54b790cff38c
Christoffer A Traeen <christoffer.andersen@live.com>
2021-03-01 12:22:30 +0100

    Change locale

--- src/components/ballot/selectBallotTypes/SelectBallotType.tsx
+++ src/components/ballot/selectBallotTypes/SelectBallotType.tsx
@@ -6,23 +6,22 @@ import SelectMultipleIcon from './SelectMultipleIcon'
 import SelectOneIcon from './SelectOneIcon'
 import SelectRankedIcon from './SelectRankedIcon'
 
-export default function SelectBallotType(): React.ReactElement {
-    const [t] = useTranslation(['common'])
+    const [t] = useTranslation(['ballot'])
 
     return (
         <div className="radio-wrapper">
             <Radio.Group className="radio-group">
                 <Radio value={1} className="radio-button">
                     <SelectMultipleIcon />
-                    <Text className="radio-text select-type-text">{t('Select multiple')}</Text>
+                        <Text className="radio-text select-type-text">{t('ballot:Select multiple')}</Text>
                 </Radio>
                 <Radio value={2} className="radio-button">
                     <SelectOneIcon />
-                    <Text className="radio-text select-type-text">{t('Select one')}</Text>
+                        <Text className="radio-text select-type-text">{t('ballot:Select one')}</Text>
                 </Radio>
                 <Radio value={3} className="radio-button">
                     <SelectRankedIcon />
-                    <Text className="radio-text select-type-text">{t('Select ranked')}</Text>
+                        <Text className="radio-text select-type-text">{t('ballot:Select ranked')}</Text>
                 </Radio>
             </Radio.Group>
         </div>
