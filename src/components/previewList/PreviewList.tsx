f3f6036491abaf339f86e883f733d065fe67b18d
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-03-02 19:43:11 +0100

    fix array type

--- src/components/previewList/PreviewList.tsx
+++ src/components/previewList/PreviewList.tsx
@@ -20,7 +20,7 @@ export default function PreviewList(): React.ReactElement {
         console.log('Not implemented')
     }
 
-    const reorder = (list: Array<any>, startIndex: number, endIndex: number) => {
+    const reorder = (list: Array<IBallotEntity>, startIndex: number, endIndex: number) => {
         const result = Array.from(list)
         const [removed] = result.splice(startIndex, 1)
         result.splice(endIndex, 0, removed)
