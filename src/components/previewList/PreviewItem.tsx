6a685699ed513b01d4e4f767840d4aa19a882526
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-03-02 12:11:36 +0100

    made draggable component

--- src/components/previewList/PreviewItem.tsx
+++ src/components/previewList/PreviewItem.tsx
@@ -2,14 +2,45 @@ import * as React from 'react'
 import Text from 'antd/lib/typography/Text'
 import { Button } from 'antd'
 import { EditFilled, CloseOutlined } from '@ant-design/icons'
+import { Draggable } from 'react-beautiful-dnd'
 
-export default function PreviewItem({ title }: { title: string }): React.ReactElement {
+export default function PreviewItem({
+    title,
+    id,
+    index,
+}: {
+    title: string
+    id: string
+    index: number
+}): React.ReactElement {
     return (
-        <div className="preview-item">
-            <Text>{title}</Text>
-            <Button type="text" icon={<EditFilled />}></Button>
+        <Draggable draggableId={id} index={index}>
+            {(provided) => (
+                <div
+                    key={id}
+                    className="preview-item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                >
+                    <Text>{title}</Text>
+                    <Button
+                        type="text"
+                        icon={<EditFilled />}
+                        onClick={() => {
+                            console.log('edit clicked')
+                        }}
+                    ></Button>
 
-            <Button type="text" icon={<CloseOutlined />}></Button>
-        </div>
+                    <Button
+                        type="text"
+                        icon={<CloseOutlined />}
+                        onClick={() => {
+                            console.log('close clicked')
+                        }}
+                    ></Button>
+                </div>
+            )}
+        </Draggable>
     )
 }
