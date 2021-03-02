22020224934f1a5a7a65e118ed2be2097286e014
Sander Hurlen <sander.hurlen@icloud.com>
2021-03-02 09:49:23 +0100

    add useSocket hook with configs moved to core

--- src/views/election/room.tsx
+++ src/views/election/room.tsx
@@ -1,17 +1,31 @@
-import React, { ReactElement } from 'react'
-import { io } from 'socket.io-client'
+import { AppConfig } from 'core/config'
+import React, { ReactElement, useEffect } from 'react'
+import useSocket from 'use-socket.io-client'
 
 export default function room(): ReactElement {
-    const socket = io('ws://192.168.2.224:8877')
+    let socket: any
+    useEffect(() => {
+        //socket = io(AppConfig.WS_URI).connect()
+        const [socket] = useSocket(AppConfig.WS_URI)
+        socket.connect()
+        socket.auth = {
+            token:
+                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwib3JnYW5pemVyIjp0cnVlLCJpYXQiOjE2MTQ2MzQ3MjYsImV4cCI6MTYxNDgwNzUyNn0.IF3gVy7mY2ffXZsHF6-isZ09QYJggdZuIogZotl8Jqk',
+        }
+        socket.on('connect', () => {
+            console.log('Connected!')
 
-    socket.on('connect', () => {
-        console.log('Connected!')
+            console.log(socket.io)
+        })
 
-        console.log(socket)
-    })
-    socket.on('pong', () => {
-        socket.send('pikk')
-    })
+        socket.on('disconnect', (data: any) => {
+            console.log('disconnected', data)
+        })
+
+        socket.on('connect_error', (data: any) => {
+            console.log('connect-error', data)
+        })
+    }, [])
 
     return (
         <div>
