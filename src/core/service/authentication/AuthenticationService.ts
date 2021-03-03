b0ddd2428ef959e9a0a035d70e11104d3132ff21
Christoffer A Traeen <christoffer.andersen@live.com>
2021-03-04 00:07:00 +0100

    Add helper method on authentication service

--- src/core/service/authentication/AuthenticationService.ts
+++ src/core/service/authentication/AuthenticationService.ts
@@ -1,16 +1,20 @@
 import { AxiosError, AxiosInstance } from 'axios'
 import { StatusCodes } from 'http-status-codes'
+import jwt from 'jwt-decode'
 import { apiRoute } from '../../routes/apiRoutes'
+import { IStorage } from '../storage/IStorage'
+import { StorageKeys } from '../storage/StorageKeys'
 import { AuthenticationDetails } from './AuthenticationDetails'
 import { AuthenticationResponse } from './AuthenticationResponse'
 import { CredentialError } from './CredentialsError'
-
+import { IToken } from './IToken'
 export class AuthenticationService {
     private _httpClient: AxiosInstance
-    private _tokenkey = 'ACCESS_TOKEN'
+    private _storageService: IStorage<StorageKeys>
 
-    constructor(httpClient: AxiosInstance) {
+    constructor(httpClient: AxiosInstance, storage: IStorage<StorageKeys>) {
         this._httpClient = httpClient
+        this._storageService = storage
     }
 
     public async authenticateOrganizer({ email, password }: AuthenticationDetails): Promise<void> {
@@ -21,8 +25,8 @@ export class AuthenticationService {
             })
             const token = response.data.token
             if (!token) throw new Error('Token not present')
-            localStorage.setItem(this._tokenkey, token)
-            this._httpClient.defaults.headers['Authorization'] = `Bearer ${token}`
+            this._storageService.setItem('ACCESS_TOKEN', token)
+            this.setAuthorization(token)
         } catch (error) {
             if (error.isAxiosError) {
                 const axiosError: AxiosError = error
@@ -32,6 +36,49 @@ export class AuthenticationService {
             }
             throw error
         }
+    }
+
+    private setAuthorization(token: string) {
+        this._httpClient.defaults.headers['Authorization'] = `Bearer ${token}`
+    }
+
+    /**
+     * Tries to login with the token stored in storage. If the token is valid
+     * return true, else false.
+     */
+    public tryLoginWithToken(): boolean {
+        if (this.hasValidAuthorizationToken()) {
+            const token = this._storageService.getItem('ACCESS_TOKEN')
+            if (token) {
+                this.setAuthorization(token)
+                return true
+            }
+        }
+        return false
+    }
+
+    /**
+     * Returns the decoded token data if a token exists, else undefined
+     */
+    public getDecodedToken(): IToken | undefined {
+        const token = this._storageService.getItem('ACCESS_TOKEN')
+        if (token) {
+            return jwt<IToken>(token)
+        }
+    }
+
+    /**
+     * Validates if we have a stored token, and the token is not expired
+     */
+    public hasValidAuthorizationToken(): boolean {
+        let isValid = false
+        const decodedToken = this.getDecodedToken()
+        if (decodedToken) {
+            // Divide Date.now by 1000 because it uses milliseconds since 1970 instead of seconds
+            // since 1970 as JWT uses
+            isValid = decodedToken.exp > Date.now() / 1000
+        }
+        return isValid
     }
 
     // !TODO Implement
