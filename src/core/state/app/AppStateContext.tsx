6039d7815d95b0a0f0619ac3e220912b0dc3d27a
Christoffer A Traeen <christoffer.andersen@live.com>
2021-03-04 00:07:31 +0100

    Setup inital state with login

--- src/core/state/app/AppStateContext.tsx
+++ src/core/state/app/AppStateContext.tsx
@@ -1,3 +1,6 @@
+import { BackendAPI } from 'core/api'
+import { AuthenticationService } from 'core/service/authentication/AuthenticationService'
+import { LocalStorageService } from 'core/service/storage/LocalStorageService'
 import React, { createContext, ReactElement, useContext, useState } from 'react'
 import { AuthLevel } from '../../service/authentication/AuthLevel'
 import { appState, IAppState } from './appState'
@@ -17,14 +20,32 @@ interface IAppStateDispatcher {
     setLogoutState: () => void
 }
 
-const appContext = createContext(appState)
+/**
+ * Setup initial state for the application when it is first run.
+ * - Set logged in state when starting the application.
+ */
+function setupInitialState() {
+    const authService = new AuthenticationService(BackendAPI, new LocalStorageService())
+    if (authService.hasValidAuthorizationToken()) {
+        if (authService.tryLoginWithToken()) {
+            const tokenData = authService.getDecodedToken()
+            appState.authLevel = tokenData?.isOrganizer ? AuthLevel.organizer : AuthLevel.voter
+            appState.isLoggedIn = true
+        }
+    }
+    return appState
+}
+
+const initialAppState = setupInitialState()
+
+const appContext = createContext(initialAppState)
 const appStateDispatch = createContext({} as IAppStateDispatcher)
 
 /**
  * Provides App state context to all child component(s) that this components wraps
  */
 function ProvideAppContext({ children }: { children: Array<ReactElement> | ReactElement }): ReactElement {
-    const [state, setState] = useState(appState)
+    const [state, setState] = useState(initialAppState)
     const dispatcher: IAppStateDispatcher = {
         setLoginState: (authLevel: AuthLevel) => {
             setState({ ...state, isLoggedIn: true, authLevel })
