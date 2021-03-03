4b2f4e5555de716e46959ee06c5cdb551c323fab
Christoffer A Traeen <christoffer.andersen@live.com>
2021-03-04 00:07:44 +0100

    Fix auth tests

--- src/core/service/authentication/AuthenticationService.test.ts
+++ src/core/service/authentication/AuthenticationService.test.ts
@@ -1,11 +1,13 @@
 import axios, { AxiosInstance } from 'axios'
+import { LocalStorageService } from 'core/service/storage/LocalStorageService'
+import { StorageKeys } from '../storage/StorageKeys'
 import { AuthenticationService } from './AuthenticationService'
 import { CredentialError } from './CredentialsError'
 const axiosMock: jest.Mocked<AxiosInstance> = (axios as unknown) as jest.Mocked<AxiosInstance>
 jest.mock('axios')
 let service!: AuthenticationService
 beforeAll(() => {
-    service = new AuthenticationService(axios)
+    service = new AuthenticationService(axios, new LocalStorageService<StorageKeys>())
 })
 
 it('should throw credential error on 400', async () => {
@@ -14,7 +16,7 @@ it('should throw credential error on 400', async () => {
             isAxiosError: true,
             response: { status: 400 },
         })
-        await service.authenticateOrganizer({ identification: 'test', password: 'test' })
+        await service.authenticateOrganizer({ email: 'test', password: 'test' })
     } catch (error) {
         expect(error).toBeInstanceOf(CredentialError)
     }
@@ -23,24 +25,32 @@ it('should throw credential error on 400', async () => {
 it('should throw error on non axios error', async () => {
     try {
         axiosMock.post.mockRejectedValue(new Error('error'))
-        await service.authenticateOrganizer({ identification: 'test', password: 'test' })
+        await service.authenticateOrganizer({ email: 'test', password: 'test' })
     } catch (error) {
         expect(error.isAxiosError).toBeUndefined()
     }
 })
 
-it('should store token if successfull login', async () => {
-    const tokenValue = 'randomtoken'
+it('should store token if successful login', async () => {
+    const tokenValue = 'randomToken'
     axiosMock.post.mockResolvedValue({ data: { token: tokenValue } })
-    await service.authenticateOrganizer({ identification: 'test', password: 'test' })
+    await service.authenticateOrganizer({ email: 'test', password: 'test' })
     expect(global.localStorage.getItem('ACCESS_TOKEN')).toBe(tokenValue)
 })
 
 it('should throw error if token is missing', async () => {
     try {
         axiosMock.post.mockResolvedValue({ data: {} })
-        await service.authenticateOrganizer({ identification: 'test', password: 'test' })
+        await service.authenticateOrganizer({ email: 'test', password: 'test' })
     } catch (error) {
         expect(error).toBeInstanceOf(Error)
     }
+})
+
+it('should be have a valid authorization token if token is not expired', async () => {
+    const tokenValue =
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhbm92b3RlIiwiaWF0IjoxNjE0ODAwOTM4LCJleHAiOjE4OTg3OTc3MzgsImF1ZCI6ImFub3ZvdGUuYXBwIiwic3ViIjoiYW5vdm90ZSIsIlJvbGUiOiJQcm9qZWN0IEFkbWluaXN0cmF0b3IifQ.FdwR1I8kPwvbkXtpYInWYwqEFPp5hqlfUwb4ArJs7ho'
+    axiosMock.post.mockResolvedValue({ data: { token: tokenValue } })
+    await service.authenticateOrganizer({ email: 'test', password: 'test' })
+    expect(service.hasValidAuthorizationToken()).toBe(true)
 })
