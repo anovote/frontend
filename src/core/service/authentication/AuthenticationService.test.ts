0bc4e4ad248c52e2ab82af104ff783e362f84d8f
Christoffer A Traeen <christoffer.andersen@live.com>
2021-03-04 11:21:24 +0100

    Add more tests to service

--- src/core/service/authentication/AuthenticationService.test.ts
+++ src/core/service/authentication/AuthenticationService.test.ts
@@ -1,14 +1,34 @@
 import axios, { AxiosInstance } from 'axios'
 import { LocalStorageService } from 'core/service/storage/LocalStorageService'
+import { IStorage } from '../storage/IStorage'
 import { StorageKeys } from '../storage/StorageKeys'
 import { AuthenticationService } from './AuthenticationService'
 import { CredentialError } from './CredentialsError'
 const axiosMock: jest.Mocked<AxiosInstance> = (axios as unknown) as jest.Mocked<AxiosInstance>
+const localStorageMock: jest.Mocked<LocalStorageService<StorageKeys>> = new LocalStorageService() as jest.Mocked<
+    LocalStorageService<StorageKeys>
+>
 jest.mock('axios')
+jest.mock('core/service/storage/LocalStorageService')
+
 let service!: AuthenticationService
+
 beforeAll(() => {
-    service = new AuthenticationService(axios, new LocalStorageService<StorageKeys>())
+    service = new AuthenticationService(axios, localStorageMock)
 })
+
+/**
+ * Contains IAT, EXP, and organizer:true and id:1
+ * Expires : 03.03.2030
+ */
+const notExpiredToken =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhbm92b3RlIiwiaWF0IjoxNjE0ODAwOTM4LCJleHAiOjE4OTg3OTc3MzgsImF1ZCI6ImFub3ZvdGUuYXBwIiwic3ViIjoiYW5vdm90ZSIsImlkIjoiMSIsIm9yZ2FuaXplciI6InRydWUifQ.OFYrMuFxasEzvMjn3phGBNUMgesrDP169P3Bz7uH_iM'
+/**
+ * Contains IAT, EXP, and organizer:true, and id:1
+ * Expires : 03.03.2021
+ */
+const expiredToken =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhbm92b3RlIiwiaWF0IjoxNjE0ODAwOTM4LCJleHAiOjE2MTQ4MDA5MzgsImF1ZCI6ImFub3ZvdGUuYXBwIiwic3ViIjoiYW5vdm90ZSIsImlkIjoiMSIsIm9yZ2FuaXplciI6InRydWUifQ.oAJFZTpmEaXD_4PriKnCnEYvcRw18cdLZPp4TIhbqRs'
 
 it('should throw credential error on 400', async () => {
     try {
@@ -31,14 +51,14 @@ it('should throw error on non axios error', async () => {
     }
 })
 
-it('should store token if successful login', async () => {
-    const tokenValue = 'randomToken'
+it('should return undefined if login successful', async () => {
+    const tokenValue = notExpiredToken
     axiosMock.post.mockResolvedValue({ data: { token: tokenValue } })
-    await service.authenticateOrganizer({ email: 'test', password: 'test' })
-    expect(global.localStorage.getItem('ACCESS_TOKEN')).toBe(tokenValue)
+    const login = await service.authenticateOrganizer({ email: 'test', password: 'test' })
+    expect(login).toBe(undefined)
 })
 
-it('should throw error if token is missing', async () => {
+it('should throw error if token is missing from response', async () => {
     try {
         axiosMock.post.mockResolvedValue({ data: {} })
         await service.authenticateOrganizer({ email: 'test', password: 'test' })
@@ -47,10 +67,50 @@ it('should throw error if token is missing', async () => {
     }
 })
 
-it('should be have a valid authorization token if token is not expired', async () => {
-    const tokenValue =
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhbm92b3RlIiwiaWF0IjoxNjE0ODAwOTM4LCJleHAiOjE4OTg3OTc3MzgsImF1ZCI6ImFub3ZvdGUuYXBwIiwic3ViIjoiYW5vdm90ZSIsIlJvbGUiOiJQcm9qZWN0IEFkbWluaXN0cmF0b3IifQ.FdwR1I8kPwvbkXtpYInWYwqEFPp5hqlfUwb4ArJs7ho'
-    axiosMock.post.mockResolvedValue({ data: { token: tokenValue } })
-    await service.authenticateOrganizer({ email: 'test', password: 'test' })
+it('token should be valid before expiration date', async () => {
+    function getItem() {
+        return notExpiredToken
+    }
+    localStorageMock.getItem.mockImplementation(getItem)
     expect(service.hasValidAuthorizationToken()).toBe(true)
+})
+
+it('token should be invalid after expiration date', async () => {
+    function getItem() {
+        return expiredToken
+    }
+    localStorageMock.getItem.mockImplementation(getItem)
+    expect(service.hasValidAuthorizationToken()).toBe(false)
+})
+
+it('should login with token if the exists a token and it is valid', async () => {
+    function getItem() {
+        return notExpiredToken
+    }
+    localStorageMock.getItem.mockImplementation(getItem)
+    expect(service.tryLoginWithToken()).toBe(true)
+})
+
+it('should not login with token if token exists but is expired', async () => {
+    function getItem() {
+        return expiredToken
+    }
+    localStorageMock.getItem.mockImplementation(getItem)
+    expect(service.tryLoginWithToken()).toBe(false)
+})
+
+it('should not login with token if token exists but is expired', async () => {
+    function getItem() {
+        return expiredToken
+    }
+    localStorageMock.getItem.mockImplementation(getItem)
+    expect(service.tryLoginWithToken()).toBe(false)
+})
+
+it('should not login with token if token does not exist', async () => {
+    function getItem() {
+        return null
+    }
+    localStorageMock.getItem.mockImplementation(getItem)
+    expect(service.tryLoginWithToken()).toBe(false)
 })
