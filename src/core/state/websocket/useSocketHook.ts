c8acef7a2e79bb1f482cab52b58c961a4ff44f59
Sander Hurlen <sander.hurlen@icloud.com>
2021-03-08 08:42:43 +0100

    refactor out attaching auth to context

--- src/core/state/websocket/useSocketHook.ts
+++ src/core/state/websocket/useSocketHook.ts
@@ -6,11 +6,9 @@ import { WebSocketContext } from './WebSocketContext'
  * A hook to fetch the socket.io instance
  * @param token the token that identifies a logged in user
  */
-const useSocket = (token: string): [AnoSocket] => {
+const useSocket = (): [AnoSocket] => {
     const socket = useContext(WebSocketContext) as AnoSocket
-    socket.auth = {
-        token: `Bearer ${token}`,
-    }
+
     return [socket]
 }
 
