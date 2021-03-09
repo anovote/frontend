12bfdef356a33ac18b29c0b92437d35f95dcb7a1
Emil <emil.nilsen9888@gmail.com>
2021-03-09 10:19:18 +0100

    Add array helpers test

---
+++ src/core/helpers/array.test.ts
@@ -0,0 +1,45 @@
+import { convertTwoDimArrayToOneDimArray, filterForDuplicates, trimItemsInArray } from './array'
+
+let testArray: string[]
+let noDuplicatesArray: string[]
+let twoDimArray: any
+
+beforeAll(() => {
+    testArray = [
+        'hello@gmail.com',
+        'hasiolhd@gmail.com',
+        '    osiadgh@lada.ru   ',
+        'hello@gmail.com',
+        'hello@gmail.com',
+        'hello@gmail.com',
+        'posjgsdpojg',
+        'oiiiiiiiiiiiiiiiii',
+    ]
+    noDuplicatesArray = ['hello@gmail.com', 'hasiolhd@gmail.com', '    osiadgh@lada.ru   ']
+    twoDimArray = [['Hello@gmail.com'], ['hasiolhd@gmail.com'], ['    osiadgh@lada.ru   ']]
+})
+
+it('should trim all items in the array', () => {
+    let copy: string[] = [...testArray]
+    copy = trimItemsInArray(copy)
+    expect(copy[2]).toBe('osiadgh@lada.ru')
+})
+
+it('should remove any duplicates in the list', () => {
+    let copy: string[] = [...testArray]
+    copy = filterForDuplicates(copy)
+    expect(copy.length).toBe(5)
+})
+
+it('should not remove any items when there are no duplicates in the list', () => {
+    let copy: string[] = [...noDuplicatesArray]
+    copy = filterForDuplicates(copy)
+    expect(copy.length).toBe(copy.length)
+})
+
+it('should convert a 2d array to a 1d array', () => {
+    const copy: any = [...twoDimArray]
+    const newArr: string[] = convertTwoDimArrayToOneDimArray(copy)
+    expect(newArr.length).toBe(3)
+    expect(newArr[0]).toBe('Hello@gmail.com')
+})
