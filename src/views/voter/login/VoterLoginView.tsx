18d69f573f65e071c9502978ebab981354f03bba
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-03-10 14:26:35 +0100

    change to use socket for confirmation

--- src/views/voter/login/VoterLoginView.tsx
+++ src/views/voter/login/VoterLoginView.tsx
@@ -1,27 +1,33 @@
 import { Alert, AlertProps, Button, Form, Input } from 'antd'
 import Layout, { Content } from 'antd/lib/layout/layout'
-import { AxiosInstance } from 'axios'
-import { BackendAPI } from 'core/api'
-import { apiRoute } from 'core/routes/apiRoutes'
-import React, { ReactElement, useReducer } from 'react'
+import { Events } from 'core/events'
+import { useSocket } from 'core/state/websocket/useSocketHook'
+import { StatusCodes } from 'http-status-codes'
+import React, { ReactElement, useEffect, useReducer } from 'react'
 import { useTranslation } from 'react-i18next'
 
 interface VoterLoginState {
     alert?: AlertProps
     isLoading: boolean
-    connectToSocket: boolean
 }
 
-type VoterLoginAction = { type: 'success' | 'sendRequest' | 'closeAlert' } | { type: 'error'; alertProps: AlertProps }
+type VoterLoginAction =
+    | { type: 'success' | 'sendRequest' | 'closeAlert' | 'connectToSocket' | 'connectedToSocket' }
+    | { type: 'error' | 'socketNotConnected' | 'emailSent'; alertProps: AlertProps }
 
 const voterLoginReducer = (state: VoterLoginState, action: VoterLoginAction) => {
     console.log(action)
     switch (action.type) {
+        case 'emailSent':
+            return { ...state, isLoading: false, alert: action.alertProps }
+
         case 'sendRequest':
             return {
                 ...state,
                 isLoading: true,
             }
+        case 'connectedToSocket':
+            return { ...state, isLoading: false }
         case 'success':
             return {
                 ...state,
@@ -38,6 +44,17 @@ const voterLoginReducer = (state: VoterLoginState, action: VoterLoginAction) => 
                 alert: action.alertProps,
                 isLoading: false,
             }
+        case 'connectToSocket':
+            return {
+                ...state,
+                isLoading: true,
+            }
+        case 'socketNotConnected':
+            return {
+                ...state,
+                isLoading: false,
+                alert: action.alertProps,
+            }
 
         default:
             break
@@ -46,7 +63,29 @@ const voterLoginReducer = (state: VoterLoginState, action: VoterLoginAction) => 
 }
 
 function VoterLoginView(): ReactElement {
+    const [socket] = useSocket()
     const [t] = useTranslation(['form', 'common'])
+
+    useEffect(() => {
+        dispatch({ type: 'connectToSocket' })
+        socket.connect()
+
+        socket.on(Events.standard.socket.connect, () => {
+            console.log(socket.id)
+            dispatch({ type: 'connectedToSocket' })
+        })
+
+        socket.on(Events.standard.socket.confirmReceivedJoin, (confirmationData: ConfirmationSocketData) => {
+            const { statusCode, message } = confirmationData
+            if (statusCode === StatusCodes.OK) {
+                dispatch({ type: 'emailSent', alertProps: { type: 'success', message } })
+            }
+        })
+
+        return () => {
+            socket.disconnect()
+        }
+    }, [])
 
     const initialState: VoterLoginState = {
         isLoading: false,
@@ -57,21 +96,17 @@ function VoterLoginView(): ReactElement {
     // todo connect to socket. wait for confirmation or disconnection
 
     const onSubmitHandler = async (form: JoinVoteDetails) => {
-        const httpClient: AxiosInstance = BackendAPI
         const { email, electionCode } = form
 
-        await sendConfirmationRequest(httpClient, email, electionCode)
+        await sendConfirmationRequest(email, electionCode)
     }
 
-    async function sendConfirmationRequest(httpClient: AxiosInstance, email: string, electionCode: string) {
+    async function sendConfirmationRequest(email: string, electionCode: string) {
         dispatch({ type: 'sendRequest' })
         try {
-            await httpClient.get(apiRoute.joinElection, {
-                params: {
-                    email,
-                    electionCode,
-                },
-            })
+            const data = { email, electionCode }
+
+            socket.emit('join', data)
         } catch (err) {
             console.log(err)
             dispatch({ type: 'error', alertProps: { type: 'error', message: err.message } })
@@ -93,24 +128,27 @@ function VoterLoginView(): ReactElement {
                 />
             )}
             <Content className="is-fullscreen has-content-center-center">
-                <Form layout="vertical" name="vote-login-form" onFinish={onSubmitHandler}>
+                <Form
+                    layout="vertical"
+                    name="vote-login-form"
+                    onFinish={onSubmitHandler}
+                    initialValues={{ email: 'tull@toys.no', electionCode: 'abc123' }}
+                >
                     <Form.Item
                         label={t('common:Email')}
-                        name={t('common:Email').toString()}
+                        //name={t('common:Email').toString()}
+                        name="email"
                         rules={[{ type: 'email', required: true, message: t('form:Email is not valid') }]}
                     >
-                        <Input
-                            disabled={state.isLoading}
-                            placeholder="email@example.com"
-                            defaultValue="test@test.com"
-                        />
+                        <Input disabled={state.isLoading} placeholder="email@example.com" />
                     </Form.Item>
                     <Form.Item
                         label={t('common:Election Code')}
-                        name={t('common:Election Code').toString()}
+                        //name={t('common:Election Code').toString()}
+                        name={'electionCode'}
                         rules={[{ type: 'string', required: true, min: 6 }]}
                     >
-                        <Input disabled={state.isLoading} defaultValue="abc123" />
+                        <Input disabled={state.isLoading} />
                     </Form.Item>
                     <Form.Item>
                         <Button type="primary" htmlType="submit" loading={state.isLoading}>
@@ -128,4 +166,9 @@ export default VoterLoginView
 interface JoinVoteDetails {
     email: string
     electionCode: string
+}
+
+interface ConfirmationSocketData {
+    message?: string
+    statusCode: StatusCodes
 }
