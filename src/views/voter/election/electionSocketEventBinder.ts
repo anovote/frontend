680af25f3ec5bd1936ddccd04b798d19b515e04a
Christoffer A Traeen <christoffer.andersen@live.com>
2021-03-10 01:19:47 +0100

    Refactor to add cleanup method

--- src/views/voter/election/electionSocketEventBinder.ts
+++ src/views/voter/election/electionSocketEventBinder.ts
@@ -8,6 +8,11 @@ let anoSocket: AnoSocket
 let electionStateDispatcher: React.Dispatch<ElectionAction>
 let hasBound = false
 
+const electionEvent = (election: IElection) => electionStateDispatcher({ type: 'election', payload: election })
+const ballotEvent = (ballot: IBallot) => electionStateDispatcher({ type: 'ballot', payload: ballot })
+const resultEvent = (result: null) => electionStateDispatcher({ type: 'result', payload: result })
+const closeEvent = (close: boolean) => electionStateDispatcher({ type: 'close', payload: close })
+
 /**
  * Binds socket events to the provided socket instance. The events are bound to the dispatcher
  * actions for the provided state dispatcher. The binding only occurs once, the first time this method
@@ -20,20 +25,23 @@ export const electionSocketEventBinder = (socket: AnoSocket, stateDispatcher: Re
         anoSocket = socket
         electionStateDispatcher = stateDispatcher
 
-        anoSocket.on(Events.voter.election, (election: IElection) =>
-            electionStateDispatcher({ type: 'election', payload: election }),
-        )
-
-        anoSocket.on(Events.voter.ballot, (ballot: IBallot) =>
-            electionStateDispatcher({ type: 'ballot', payload: ballot }),
-        )
-
-        anoSocket.on(Events.voter.result, (result: null) =>
-            electionStateDispatcher({ type: 'result', payload: result }),
-        )
-
-        anoSocket.on(Events.voter.close, (close: boolean) => electionStateDispatcher({ type: 'close', payload: close }))
+        anoSocket.on(Events.voter.election, electionEvent)
+        anoSocket.on(Events.voter.ballot, ballotEvent)
+        anoSocket.on(Events.voter.result, resultEvent)
+        anoSocket.on(Events.voter.close, closeEvent)
 
         hasBound = true
+    }
+}
+
+/**
+ * Remove all events for the election
+ */
+export const electionSocketEventCleanup = (): void => {
+    if (anoSocket) {
+        anoSocket.removeEventListener(Events.voter.election, electionEvent)
+        anoSocket.removeEventListener(Events.voter.ballot, ballotEvent)
+        anoSocket.removeEventListener(Events.voter.result, resultEvent)
+        anoSocket.removeEventListener(Events.voter.close, closeEvent)
     }
 }
