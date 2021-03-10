e5b30a9d51c01623913005f976b57806b8847a1a
Christoffer A Traeen <christoffer.andersen@live.com>
2021-03-10 18:01:05 +0100

    Organize css classes

--- src/components/voterContentInfo/VoterContentInfo.tsx
+++ src/components/voterContentInfo/VoterContentInfo.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react'
 
 export default function VoterContentInfo({ context, title }: { title: string; context?: string }): ReactElement {
     return (
-        <div className="election-top">
+        <div className="voter-election-top">
             {!!context && <span className="text-label">{context}</span>}
             <h2 className="text-medium">{title}</h2>
         </div>
--- src/components/voterHeader/VoterHeader.tsx
+++ src/components/voterHeader/VoterHeader.tsx
@@ -3,8 +3,8 @@ import React, { ReactElement } from 'react'
 import { ReactComponent as AnovoteLogo } from 'style/assets/logo-icon.svg'
 export default function VoterHeader({ slogan = '...' }: { slogan?: string }): ReactElement {
     return (
-        <Header className="voter-header">
-            <AnovoteLogo className={'header-logo'} />
+        <Header className="voter-election-header">
+            <AnovoteLogo className={'voter-header-logo'} />
             <span className="text-label">/</span>
             <span className="header-slogan">{slogan}</span>
         </Header>
