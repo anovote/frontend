7f4eaed85362c01cbc707fd7b354bbd1f72c3ce3
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-03-12 14:49:25 +0100

    extract new component

---
+++ src/views/election/election/ElectionStatusCard.tsx
@@ -0,0 +1,107 @@
+import {
+    ClockCircleOutlined,
+    EyeInvisibleOutlined,
+    EyeOutlined,
+    ForwardOutlined,
+    LockOutlined,
+    SafetyOutlined,
+    SecurityScanOutlined,
+    UnlockOutlined,
+} from '@ant-design/icons'
+import { Space } from 'antd'
+import Title from 'antd/lib/typography/Title'
+import CardList from 'components/cards/CardList'
+import CountUpTimer from 'components/countUpTimer/countUpTimer'
+import { IElection } from 'core/models/IElection'
+import React, { ReactElement, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { IStatusDetail } from './IStatusDetail'
+import StatusListItem from './StatusListItem'
+
+export function ElectionStatusCard({ election }: { election: IElection }): ReactElement {
+    const [t] = useTranslation(['common'])
+
+    const header = (
+        <div className="spread align-items-center">
+            <Title level={2}>{t('common:Status')} </Title>
+            <div className="">
+                <span className="text-label">{t('common:Election').toUpperCase()} ID # </span>
+                <span className="text-medium">{election.id}</span>
+            </div>
+        </div>
+    )
+
+    const details: Array<IStatusDetail> = [
+        {
+            icon: <LockOutlined />,
+            colorClass: 'success-light',
+            title: t('election:Election open'),
+            text: election.openDate ? (
+                election.openDate.toLocaleDateString
+            ) : (
+                <span className="field-not-relevant"> {new Date().toLocaleDateString()}</span>
+            ),
+        },
+        {
+            icon: <UnlockOutlined />,
+            colorClass: 'danger-light',
+            title: t('election:Election close'),
+            text: election.closeDate ? (
+                election.closeDate
+            ) : (
+                <span className="field-not-relevant"> {new Date().toLocaleDateString()}</span>
+            ),
+        },
+        {
+            icon: <ClockCircleOutlined />,
+            colorClass: 'main-light',
+            title: t('election:Time elapsed'),
+            // TODO! implement logic to set timer on states from current election
+            text: <CountUpTimer />,
+        },
+        {
+            icon: <ForwardOutlined />,
+            colorClass: 'main-light',
+            title: t('election:Ballot proceed'),
+            text: election.isAutomatic ? 'automatic' : 'manual',
+        },
+        {
+            icon: <SecurityScanOutlined />,
+            colorClass: 'main-light',
+            title: t('election:Authentication method'),
+            text: t('common:Email'),
+        },
+        {
+            icon: <SafetyOutlined />,
+            colorClass: 'main-light',
+            title: t('common:Password'),
+
+            text: <PasswordShowHide password={'Anovote101!'} />,
+        },
+    ]
+
+    return <CardList listHeader={header} list={details} renderItem={(item) => StatusListItem(item)}></CardList>
+}
+
+export function PasswordShowHide({ password }: { password: string }): ReactElement {
+    const [hide, setHide] = useState(true)
+    const [toggleHideOn, setToggleHideOn] = useState(false)
+
+    const hiddenPassword = '••••••••'
+
+    const toggleHide = () => {
+        //setHide(!hide)
+        setToggleHideOn(!toggleHideOn)
+    }
+
+    return (
+        <>
+            <Space>
+                <text onClick={toggleHide} onMouseEnter={() => setHide(false)} onMouseLeave={() => setHide(true)}>
+                    {hide && !toggleHideOn ? hiddenPassword : password}
+                </text>
+                <span onClick={toggleHide}>{!toggleHideOn ? <EyeInvisibleOutlined /> : <EyeOutlined />}</span>
+            </Space>
+        </>
+    )
+}
--- src/views/election/election/index.tsx
+++ src/views/election/election/index.tsx
@@ -12,63 +12,12 @@ import CardList from 'components/cards/CardList'
 import CountUpTimer from 'components/countUpTimer/countUpTimer'
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
-import { IStatusDetail } from './IStatusDetail'
-import StatusListItem from './StatusListItem'
+import { ElectionStatusCard } from './ElectionStatusCard'
 /**
  * The main view used for creating an election
  */
 export default function ElectionView(): React.ReactElement {
     const [t] = useTranslation(['translation', 'common', 'election'])
-    const header = (
-        <div className="spread align-items-center">
-            <Title level={2}>{t('common:Status')} </Title>
-            <div className="">
-                <span className="text-label">{t('common:Election').toUpperCase()} ID # </span>
-                <span className="text-medium">123456</span>
-            </div>
-        </div>
-    )
-
-    const details: Array<IStatusDetail> = [
-        {
-            icon: <LockOutlined />,
-            colorClass: 'success-light',
-            title: t('election:Election open'),
-            text: new Date().toLocaleDateString(),
-        },
-        {
-            icon: <UnlockOutlined />,
-            colorClass: 'danger-light',
-            title: t('election:Election close'),
-            text: new Date().toLocaleDateString(),
-        },
-        {
-            icon: <ClockCircleOutlined />,
-            colorClass: 'main-light',
-            title: t('election:Time elapsed'),
-            // TODO! implement logic to set timer on states from current election
-            text: <CountUpTimer />,
-        },
-        {
-            icon: <ForwardOutlined />,
-            colorClass: 'main-light',
-            title: t('election:Ballot proceed'),
-            text: 'automatic',
-        },
-        {
-            icon: <SecurityScanOutlined />,
-            colorClass: 'main-light',
-            title: t('election:Authentication method'),
-            text: 'Feide, Google',
-        },
-        {
-            icon: <SafetyOutlined />,
-            colorClass: 'main-light',
-            title: t('common:Password'),
-
-            text: 'Anovote101!',
-        },
-    ]
 
     const cardTitle = <Title level={2}>{t('election:Connected voters')}</Title>
     return (
@@ -76,7 +25,7 @@ export default function ElectionView(): React.ReactElement {
             {/*<Row>*/}
             <Col>
                 <Space direction={'vertical'}>
-                    <CardList listHeader={header} list={details} renderItem={(item) => StatusListItem(item)}></CardList>
+                        <ElectionStatusCard election={elections[0]} />
                     <Card className={'info-card'} title={cardTitle}>
                         <div className="is-flex-column has-content-center-center">
                             <span className={'text-large'}>1337</span> {/* todo fetch real time*/}
