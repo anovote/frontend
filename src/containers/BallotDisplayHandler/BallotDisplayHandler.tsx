7396099b6d3fd1d4888725b3caf3c371ad42dfaf
Sander Hurlen <sander.hurlen@icloud.com>
2021-03-14 17:37:54 +0100

    add components for displaying ballot

---
+++ src/containers/BallotDisplayHandler/BallotDisplayHandler.tsx
@@ -0,0 +1,103 @@
+import { Button, Space } from 'antd'
+import { CheckboxValueType } from 'antd/lib/checkbox/Group'
+import { RadioChangeEvent } from 'antd/lib/radio'
+import Title from 'antd/lib/typography/Title'
+import CandidateList from 'components/CandidateList/CandidateList'
+import { BallotType } from 'core/models/ballot/BallotType'
+import { IBallot } from 'core/models/ballot/IBallot'
+import React, { ReactElement, useReducer, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+type State = {
+    selected: number
+}
+
+type Action = { type: 'select' } | { type: 'deselect' } | { type: 'reset' }
+
+const reducer = (state: State, action: Action): State => {
+    switch (action.type) {
+        case 'select':
+            return { selected: state.selected + 1 }
+        case 'deselect':
+            return { selected: state.selected - 1 }
+        case 'reset':
+            return { selected: 0 }
+        default:
+    }
+    return { selected: state.selected }
+}
+
+const initialState = { selected: 0 }
+
+export default function BallotDisplayHandler({ ballot }: { ballot: IBallot }): ReactElement {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const [selection, setSelection] = useState<{ single: number | null; multiple: number[] }>({
+        single: null,
+        multiple: [],
+    })
+
+    const [t] = useTranslation(['common'])
+
+    /**
+     * Handles the change of clicked candidate(s) according to
+     * the type of the ballot
+     * @param change the selected change of candidate(s)
+     */
+    const onChange = (change: RadioChangeEvent | CheckboxValueType[]) => {
+        switch (ballot.type) {
+            case BallotType.SINGLE: {
+                const radio = change as RadioChangeEvent
+                if (state.selected != 1) {
+                    dispatch({ type: 'select' })
+                }
+                setSelection({ single: radio.target.value, multiple: [] })
+                break
+            }
+            case BallotType.MULTIPLE: {
+                const checked = change as number[]
+                if (checked.length > state.selected) {
+                    dispatch({ type: 'select' })
+                }
+                if (checked.length < state.selected) {
+                    dispatch({ type: 'deselect' })
+                }
+                setSelection({ single: null, multiple: checked })
+                break
+            }
+
+            default:
+                console.error('Ballot type not handled')
+        }
+    }
+
+    /**
+     * Clears the selected/selection of candidates and resets the selected
+     * counter
+     */
+    const clearSelection = () => {
+        setSelection({ single: null, multiple: [] })
+        dispatch({ type: 'reset' })
+    }
+
+    return (
+        <div className="voter-display">
+            <Space className="width-100 spread">
+                <Title level={5}>
+                    {t('common:Candidate_plural')}: {ballot.candidates.length}
+                </Title>
+                <Title level={5}>
+                    {t('common:Selected')} ({state.selected})
+                </Title>
+                <Button onClick={clearSelection} type="text" danger={true} style={{ opacity: !state.selected ? 0 : 1 }}>
+                    {t('common:Clear')}
+                </Button>
+            </Space>
+            <CandidateList
+                typeOfSelection={ballot.type}
+                candidates={ballot.candidates}
+                onChange={onChange}
+                selection={ballot.type == BallotType.SINGLE ? selection.single : selection.multiple}
+            />
+        </div>
+    )
+}
