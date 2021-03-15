7a1d3ff3fbbeebd08f7634870b48112710db1528
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-03-15 14:55:58 +0100

    fix election title

--- src/views/election/ElectionInProgressView.tsx
+++ src/views/election/ElectionInProgressView.tsx
@@ -1,8 +1,8 @@
+import Title from 'antd/lib/typography/Title'
 import BallotsQueue from 'components/queue/BallotsQueue'
 import { IBallotEntity } from 'core/models/ballot/IBallotEntity'
 import { IElectionDetails } from 'core/service/election/IElectionDetails'
 import { useSocket } from 'core/state/websocket/useSocketHook'
-import { freshBallots } from 'dummy/ballotsDummyData'
 import React, { ReactElement, useEffect } from 'react'
 
 export function ElectionInProgressView({ election }: { election: IElectionDetails }): ReactElement {
@@ -25,7 +25,7 @@ export function ElectionInProgressView({ election }: { election: IElectionDetail
 
     return (
         <>
-            <h1>{election.title}</h1>
+            <Title level={1}>{election.title}</Title>
             <BallotsQueue dataSource={ballots} />
         </>
     )
