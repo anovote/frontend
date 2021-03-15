948e46c3c90b4d4b84a6d96cecbdb614452a0431
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-03-15 10:52:42 +0100

    add conditional rendering based on status

--- src/views/election/election/index.tsx
+++ src/views/election/election/index.tsx
@@ -4,6 +4,7 @@ import { ElectionStatus } from 'core/models/ElectionStatus'
 import { IElection } from 'core/models/IElection'
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
+import CreateElectionView from '../createElection'
 import { ElectionInProgressView } from '../ElectionInProgressView'
 import { ElectionStatusCard } from './ElectionStatusCard'
 
@@ -14,8 +15,9 @@ const elections: IElection[] = [
         description: 'Very nice voting',
         title: 'My election',
         isAutomatic: false,
-        status: ElectionStatus.Started,
+        status: ElectionStatus.NotStarted,
         isLocked: false,
+        password: 'get it done',
     },
 ]
 
@@ -25,6 +27,19 @@ const elections: IElection[] = [
 export default function ElectionView(): React.ReactElement {
     const [t] = useTranslation(['translation', 'common', 'election'])
     const election = elections[0]
+
+    const renderElectionView = (electionStatus: ElectionStatus) => {
+        switch (electionStatus) {
+            case ElectionStatus.NotStarted:
+                return <CreateElectionView initElection={election} />
+            case ElectionStatus.Started:
+                return <ElectionInProgressView />
+            case ElectionStatus.Finished:
+                return 'Election finished'
+            default:
+                return null
+        }
+    }
 
     const cardTitle = <Title level={2}>{t('election:Connected voters')}</Title>
     return (
@@ -40,7 +55,7 @@ export default function ElectionView(): React.ReactElement {
                         </Card>
                     </Space>
                 </Col>
-                <Col span={12}>{election.status === ElectionStatus.Started && <ElectionInProgressView />}</Col>
+                <Col span={16}>{renderElectionView(election.status)}</Col>
             </Row>
         </>
     )
