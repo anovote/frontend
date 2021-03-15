1906e46810fa459761a70439f1a7d371ead39afb
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-03-15 14:40:28 +0100

    rename component

--- src/components/election/CreateElectionButton.tsx
+++ src/components/election/CreateElectionButton.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Button, Form } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
 
-export default function CreateElectionButton({ hasInitial = false }: { hasInitial?: boolean }): React.ReactElement {
+export default function SaveElectionButton({ hasInitial = false }: { hasInitial?: boolean }): React.ReactElement {
     return (
         <div>
             <Form.Item>
