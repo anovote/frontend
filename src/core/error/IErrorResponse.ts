fd3d7b3e9cfebeee4f92fee637267147dcd97f41
Christoffer A Traeen <christoffer.andersen@live.com>
2021-03-15 17:55:18 +0100

    Add status code to error response

--- src/core/error/IErrorResponse.ts
+++ src/core/error/IErrorResponse.ts
@@ -4,8 +4,10 @@
 export interface IErrorResponse {
     // A descriptive message of the error
     message: string
-    // The HTTP status code of the error > 400, 403, 500
+    // The HTTP status name of the error > FORBIDDEN, BAD_REQUEST...
     status: string
+    // The HTTP status code of the error > 400, 404, 500...
+    statusCode: number
     // A more precise error code to pinpoint the error
     code: string
 }
