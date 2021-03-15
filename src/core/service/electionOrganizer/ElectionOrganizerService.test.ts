4dedd5ff171314aa4c658681bd3caa83ee5064f7
Sander Hurlen <sander.hurlen@icloud.com>
2021-03-15 16:03:28 +0100

    refactor out own errors folder

--- src/core/service/electionOrganizer/ElectionOrganizerService.test.ts
+++ src/core/service/electionOrganizer/ElectionOrganizerService.test.ts
@@ -1,8 +1,8 @@
 import { ElectionOrganizerService } from './ElectionOrganizerService'
-import { PasswordDoesNotMatchError, PasswordIsNotValidError } from '../../models/customErrors'
-import { ChangePasswordInterface } from '../../../views/changePassword/ChangePassword'
+import { PasswordDoesNotMatchError, PasswordIsNotValidError } from 'core/errors/customErrors'
 import axios, { AxiosInstance } from 'axios'
 import RandExp from 'randexp'
+import { ChangePasswordInterface } from 'containers/forms/profile/ChangePasswordForm'
 
 const axiosMock: jest.Mocked<AxiosInstance> = (axios as unknown) as jest.Mocked<AxiosInstance>
 jest.mock('axios')
@@ -10,19 +10,19 @@ let passwords: ChangePasswordInterface
 
 const es = new ElectionOrganizerService(axiosMock)
 it('should resolve if password matches criteria', async () => {
-    let password = '!Dest123'
+    const password = '!Dest123'
     passwords = { password1: password, password2: password }
     await expect(es.validateAndChangePassword(passwords)).resolves.toBe(undefined)
 })
 it('should throw password is not valid if password does not match criteria', async () => {
-    let password = '1Dest123'
+    const password = '1Dest123'
     passwords = { password1: password, password2: password }
     await expect(es.validateAndChangePassword(passwords)).rejects.toThrowError(PasswordIsNotValidError)
 })
 
 it('throws error if password is not equal', async () => {
     const password1 = 'notEqu1!s'
-    let password2 = 'toTh!5sks'
+    const password2 = 'toTh!5sks'
     passwords = { password1, password2 }
     await expect(es.validateAndChangePassword(passwords)).rejects.toThrowError(PasswordDoesNotMatchError)
 })
@@ -45,7 +45,7 @@ it('should throw PasswordIsNotValidError if password is empty or less than 8', a
 })
 //http://fent.github.io/randexp.js/#r=%5Ba-zA-Z1-9%21@%23%25%5E%26*%5D%7B226%7D&i
 it('should throw PasswordIsNotValidError if password is longer than 225 characters', async () => {
-    let password = new RandExp('[a-zA-Z1-9!@#%^&*]{226}').gen()
+    const password = new RandExp('[a-zA-Z1-9!@#%^&*]{226}').gen()
     passwords = { password1: password, password2: password }
     await expect(es.validateAndChangePassword(passwords)).rejects.toThrowError(PasswordIsNotValidError)
 })
