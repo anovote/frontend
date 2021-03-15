1e1a62b044ed54d58f0fa8181bd274128b834f21
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-03-15 12:20:02 +0100

    merge interfaces

--- src/core/service/election/IElectionDetails.ts
+++ src/core/service/election/IElectionDetails.ts
@@ -1,14 +1,17 @@
+import { IBallot } from 'core/models/ballot/IBallot'
 import { IEligibleVoter } from 'core/models/ballot/IEligibleVoter'
 import { ElectionStatus } from './ElectionStatus'
 
+// todo check if this is needed ? IElection might be a duplicate
 export interface IElectionDetails {
     title: string
     description: string
-    openDate: Date
-    closeDate: Date
-    eligibleVoters: IEligibleVoter[]
+    openDate?: Date
+    closeDate?: Date
+    eligibleVoters?: IEligibleVoter[]
     password?: string
     status: ElectionStatus
     isLocked: boolean
     isAutomatic: boolean
+    ballots?: IBallot[]
 }
