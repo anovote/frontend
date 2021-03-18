575b556253a43109f4084be8ddef17bb6e60a1cf
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-03-18 13:06:47 +0100

    add route

--- src/core/routes/apiRoutes.ts
+++ src/core/routes/apiRoutes.ts
@@ -1,6 +1,7 @@
 export const apiRoute = {
     authentication: '/public/auth/login',
     createElection: '/admin/elections',
+    getElection: '/admin/elections/',
     electionOrganizer: '/admin/electionOrganizer',
     registration: '/public/auth/register',
     ballots: '/ballots',
