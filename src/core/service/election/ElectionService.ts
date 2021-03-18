747addde5fadba3499ae2029e774e7c7e68a2ade
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-03-18 13:07:20 +0100

    add getter

--- src/core/service/election/ElectionService.ts
+++ src/core/service/election/ElectionService.ts
@@ -1,4 +1,5 @@
 import { AxiosError, AxiosInstance } from 'axios'
+import { IElection } from 'core/models/IElection'
 import { StatusCodes } from 'http-status-codes'
 import { apiRoute } from '../../routes/apiRoutes'
 import { AuthorizationError } from './AuthorizationError'
@@ -21,6 +22,7 @@ export class ElectionService {
         status,
         isLocked,
         isAutomatic,
+        ballots,
     }: IElectionDetails): Promise<void> {
         try {
             await this.httpClient.post(apiRoute.createElection, {
@@ -33,8 +35,26 @@ export class ElectionService {
                 status,
                 isLocked,
                 isAutomatic,
+                ballots,
             })
             // TODO handle what to do with the response
+        } catch (error) {
+            if (error.isAxiosError) {
+                const axiosError: AxiosError = error
+                if (axiosError.response?.status === StatusCodes.UNAUTHORIZED) {
+                    throw new AuthorizationError('You need to be logged in to create an election!')
+                }
+                if (axiosError.response?.status === StatusCodes.INTERNAL_SERVER_ERROR) {
+                    throw new Error('Error at the server, drink some Tea and wait')
+                }
+            }
+            throw error
+        }
+    }
+
+    public async getElection(electionId: number): Promise<IElection> {
+        try {
+            return await (await this.httpClient.get(`${apiRoute.getElection}${electionId}`)).data
         } catch (error) {
             if (error.isAxiosError) {
                 const axiosError: AxiosError = error
