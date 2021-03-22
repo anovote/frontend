a2a220ea101bde88b60b192bcfb685ff714b010a
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-03-22 21:24:50 +0100

    add logout button

--- src/containers/modal/ProfileSettingsModal.tsx
+++ src/containers/modal/ProfileSettingsModal.tsx
@@ -1,10 +1,16 @@
-import { Col, Row } from 'antd'
+import { AlertProps, Button, Col, Row } from 'antd'
 import Modal from 'antd/lib/modal/Modal'
 import Title from 'antd/lib/typography/Title'
 import ChangeEmailFrom from 'containers/forms/profile/ChangeEmailForm'
 import ChangePasswordForm from 'containers/forms/profile/ChangePasswordForm'
+import { BackendAPI } from 'core/api'
+import { getPublicRoute } from 'core/routes/siteRoutes'
+import { AuthenticationService } from 'core/service/authentication/AuthenticationService'
+import { LocalStorageService } from 'core/service/storage/LocalStorageService'
+import { AlertState } from 'core/state/AlertState'
 import React, { ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useHistory } from 'react-router'
 
 export default function ProfileSettingsModal({
     showModal,
@@ -14,6 +20,14 @@ export default function ProfileSettingsModal({
     close: () => void
 }): ReactElement {
     const [t] = useTranslation(['translation', 'common', 'form', 'profile'])
+    const history = useHistory<AlertState>()
+
+    const logoutHandler = () => {
+        console.log('here')
+        new AuthenticationService(BackendAPI, new LocalStorageService()).logout()
+        const alert: AlertProps = { message: 'You where logged out', closable: true, type: 'info', showIcon: true }
+        history.push(getPublicRoute().login, { alertProps: alert })
+    }
 
     return (
         <>
@@ -40,6 +54,13 @@ export default function ProfileSettingsModal({
                     <Col span={24}>
                         <Title level={3}>{t('profile:Change password')}</Title>
                         <ChangePasswordForm />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button className="logout-btn" onClick={logoutHandler}>
+                            {t('common:Log out')}
+                        </Button>
                     </Col>
                 </Row>
             </Modal>
