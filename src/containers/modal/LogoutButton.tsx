912199df8a5a45ac5ae2ace0acd34fbdea7c6c64
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-03-22 21:37:28 +0100

    add return type

--- src/containers/modal/LogoutButton.tsx
+++ src/containers/modal/LogoutButton.tsx
@@ -4,11 +4,11 @@ import { getPublicRoute } from 'core/routes/siteRoutes'
 import { AuthenticationService } from 'core/service/authentication/AuthenticationService'
 import { LocalStorageService } from 'core/service/storage/LocalStorageService'
 import { AlertState } from 'core/state/AlertState'
-import React from 'react'
+import React, { ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useHistory } from 'react-router'
 
-export function LogoutButton() {
+export function LogoutButton(): ReactElement {
     const history = useHistory<AlertState>()
 
     const [t] = useTranslation()
