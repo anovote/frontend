6ee4bdf1cd56d443bb1c7b1662628c6f4544acf4
Sander Hurlen <sander.hurlen@icloud.com>
2021-04-08 15:34:13 +0200

    refactor to be reactive

--- src/components/ConnectionIndicator/ConnectionIndicator.tsx
+++ src/components/ConnectionIndicator/ConnectionIndicator.tsx
@@ -1,7 +1,8 @@
 import { DisconnectOutlined, RocketOutlined } from '@ant-design/icons'
 import { Popover } from 'antd'
+import { Events } from 'core/events'
 import { useSocket } from 'core/hooks/useSocket'
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 export default function ConnectionIndicator(): ReactElement {
@@ -18,23 +19,31 @@ export default function ConnectionIndicator(): ReactElement {
         </div>
     )
 
-    let renderIndicator = (
+    const disconnectedElement = (
         <>
             <Popover placement="bottomRight" content={disconnectedContent} title={t('common:Disconnected')}>
                 <DisconnectOutlined />
             </Popover>
         </>
     )
+    const connectedElement = (
+        <>
+            <Popover placement="bottomRight" content={connectedContent} title={t('common:Connected')}>
+                <RocketOutlined />
+            </Popover>
+        </>
+    )
+    const [renderIndicator, setRenderIndicator] = useState(disconnectedElement)
 
-    if (socket.connected) {
-        renderIndicator = (
-            <>
-                <Popover placement="bottomRight" content={connectedContent} title={t('common:Connected')}>
-                    <RocketOutlined />
-                </Popover>
-            </>
-        )
-    }
+    useEffect(() => {
+        socket.on(Events.standard.socket.connect, () => {
+            setRenderIndicator(connectedElement)
+        })
+
+        socket.on(Events.standard.socket.disconnect, () => {
+            setRenderIndicator(disconnectedElement)
+        })
+    }, [socket])
 
     return <div className="connection-indicator">{renderIndicator}</div>
 }
