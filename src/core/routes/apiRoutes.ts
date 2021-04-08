6aa99ea5ae6cd4d9387779e1a89f0e24543b1d51
Christoffer A Traeen <christoffer.andersen@live.com>
2021-04-08 02:15:24 +0200

    Create api route methods

--- src/core/routes/apiRoutes.ts
+++ src/core/routes/apiRoutes.ts
@@ -1,40 +1,92 @@
-// TODO Refactor this
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 export const apiRoute = {
     authentication: '/public/auth/login',
-    createElection: '/admin/elections',
-    getElection: '/admin/elections/',
-    electionOrganizer: '/admin/electionOrganizer',
+    createElection: '/admin/elections', //
+    getElection: '/admin/elections/', //
+    electionOrganizer: '/admin/electionOrganizer', //
     registration: '/public/auth/register',
-    ballots: '/ballots',
+    ballots: '/ballots', //
     joinElection: '/join',
 }
 
+/**
+ * Creates a path string, and returns an append function
+ * that returns a new path creator object and a get function to get the current path.
+ * Forward slash is prefixed on create and when appending
+ * @param part part to start with
+ * @param preSlash prepend path with slash
+ * @returns return append function or getter for current path
+ */
+function createPath(part: string, preSlash = true) {
+    const path = `${preSlash ? '/' : ''}${part}`
+
+    // Returns the current path
+    function get() {
+        return path
+    }
+    // Appends a new part to the current path and returns a new new path object
+    function append(addPart: string | number) {
+        return createPath(`${path}/${addPart}`, false)
+    }
+
+    return {
+        append,
+        get,
+    }
+}
 /**
  * Admin routes
  */
 const adminPrefix = 'admin'
 const adminRoute = () => {
-    function create(path: string) {
-        return `/${adminPrefix}/${path}`
-    }
+    const path = createPath(`${adminPrefix}`)
     return {
-        election: {
-            getById: (id: number) => create(`elections/${id}`),
+        get: path.get(),
+        election: () => {
+            const electionPath = path.append('elections')
+            return {
+                create: electionPath.get(),
+                byId: (id: number) => {
+                    const electionIdPath = electionPath.append(`${id}`)
+                    return {
+                        get: electionIdPath.get(),
+                        update: electionIdPath.get(),
+                        ballots: () => {
+                            const ballotsPath = electionIdPath.append('ballots')
+                            return {
+                                get: ballotsPath.get(),
+                            }
+                        },
+                    }
+                },
+            }
+        },
+        organizer: () => {
+            const organizerPath = path.append('electionOrganizer')
+            return {
+                update: organizerPath.get(),
+            }
         },
     }
 }
-
 /**
  * Voter routes
  */
 const voterPrefix = 'voter'
 const voterRoute = () => {
-    function create(path: string) {
-        return `/${voterPrefix}/${path}`
-    }
+    const path = createPath(voterPrefix)
     return {
-        elections: {
-            getById: (id: number) => create(`elections/${id}`),
+        get: path.get(),
+        election: () => {
+            const electionPath = path.append('elections')
+            return {
+                byId: (id: number) => {
+                    const electionIdPath = electionPath.append(id)
+                    return {
+                        get: electionIdPath.get(),
+                    }
+                },
+            }
         },
     }
 }
@@ -44,11 +96,16 @@ const voterRoute = () => {
  */
 const publicPrefix = 'public'
 const publicRoute = () => {
-    function create(path: string) {
-        return `/${publicPrefix}/${path}`
-    }
+    const path = createPath(publicPrefix)
     return {
-        auth: { join: create('join') },
+        get: path.get(),
+        auth: () => {
+            const authPath = path.append('auth')
+            return {
+                register: authPath.append('register').get(),
+                login: authPath.append('login').get(),
+            }
+        },
     }
 }
 
