0fcc78ef0fc0250abf49d187c3c7504855446459
Sander Hurlen <sander.hurlen@icloud.com>
2021-04-13 13:07:20 +0200

    fix pr comments

--- src/components/overrides/DatePicker.tsx
+++ src/components/overrides/DatePicker.tsx
@@ -1,12 +1,3 @@
-//import { Dayjs } from 'dayjs'
-//import dayjsGenerateConfig from 'rc-picker/lib/generate/dayjs'
-//import generatePicker from 'antd/es/date-picker/generatePicker'
-//import 'antd/es/date-picker/style/index'
-
-//const DatePicker = generatePicker<Dayjs>(dayjsGenerateConfig)
-
-//export default DatePicker
-
 import dateFnsGenerateConfig from 'rc-picker/lib/generate/dateFns'
 import generatePicker from 'antd/es/date-picker/generatePicker'
 import 'antd/es/date-picker/style/index'
