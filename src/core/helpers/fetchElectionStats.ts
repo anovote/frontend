928ea46e3cfd00f07680a44f5ec06332cd825660
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-04-14 13:08:57 +0200

    fix api route

--- src/core/helpers/fetchElectionStats.ts
+++ src/core/helpers/fetchElectionStats.ts
@@ -1,10 +1,10 @@
 import { BackendAPI } from 'core/api'
 import { IBallotStats } from 'core/models/ballot/IBallotStats'
-import { getAdminRoute } from 'core/routes/siteRoutes'
+import { apiRoute } from 'core/routes/apiRoutes'
 import { StatusCodes } from 'http-status-codes'
 
 export const fetchElectionStats = async (electionId: number): Promise<IBallotStats[]> => {
-    const url = `${getAdminRoute().elections.view}/${electionId}/stats`
+    const url = `${apiRoute.getElection}/${electionId}/stats`
     try {
         const response = await BackendAPI.get<IBallotStats[]>(url)
         if (response.status !== StatusCodes.OK) {
