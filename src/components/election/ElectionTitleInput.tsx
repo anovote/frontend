eaf1a2abf56fbf53c7e7940be8701dab13218785
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-04-20 12:15:15 +0200

    fix translation

--- src/components/election/ElectionTitleInput.tsx
+++ src/components/election/ElectionTitleInput.tsx
@@ -1,8 +1,10 @@
-import * as React from 'react'
 import { Form, Input } from 'antd'
+import * as React from 'react'
+import { useTranslation } from 'react-i18next'
 export default function ElectionTitleInput(): React.ReactElement {
+    const [t] = useTranslation('form')
     return (
-        <Form.Item name="title" rules={[{ required: true, message: 'Please fill in a title!' }]}>
+        <Form.Item name="title" rules={[{ required: true, message: t('form:Please fill in a title') }]}>
             <Input placeholder="Title"></Input>
         </Form.Item>
     )
