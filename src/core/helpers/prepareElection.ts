d453da321c0070c6548f78a8a35710d067a1e141
Sander Hurlen <sander.hurlen@icloud.com>
2021-04-23 12:06:26 +0200

    fix ci errors

--- src/core/helpers/prepareElection.ts
+++ src/core/helpers/prepareElection.ts
@@ -1,14 +1,14 @@
 import { IElection } from 'core/models/election/IElection'
 
 /**
- * Prepares an election for an form by fixing values that is transformed via the JSON oject. For instance, an
+ * Prepares an election for an form by fixing values that is transformed via the JSON object. For instance, an
  * date object on server is provided as a date string over JSON.
  * Since the form handling of ant d uses objects and not classes, it makes it difficult for use to work with our classes
  * Especially since the date for date-fns should use date instances and not strings (which is what we get from the server)
  * @param election the election object to transform
  * @returns a new Election object with proper values for fields
  */
-export const prepareElection = (election: IElection) => {
+export const prepareElection = (election: IElection): IElection => {
     if (election.openDate && !(election.openDate instanceof Date)) {
         election.openDate = new Date(election.openDate)
     }
