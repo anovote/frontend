caa43cbdeb68192c51c9a6de22d83dbef08b9533
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-04-26 12:24:25 +0200

    fix t return undefined

--- src/core/helpers/formRules.ts
+++ src/core/helpers/formRules.ts
@@ -1,5 +1,7 @@
 import { Rule } from 'antd/lib/form'
-import i18next from 'i18next'
+import i18n from 'i18n'
+
+i18n.loadNamespaces('form')
 
 const PASSWORD_MAX_LENGTH = 255
 const PASSWORD_MIN_LENGTH = 8
@@ -7,32 +9,32 @@ const strongRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*
 
 export const formRules: IFormRules = {
     password: [
-        { max: PASSWORD_MAX_LENGTH, message: i18next.t('form:max-length', { maxLength: PASSWORD_MAX_LENGTH }) },
-        { required: true, message: i18next.t('form:Remember password') },
+        { max: PASSWORD_MAX_LENGTH, message: i18n.t('form:max-length', { maxLength: PASSWORD_MAX_LENGTH }) },
+        { required: true, message: i18n.t('form:Remember password') },
         {
             min: PASSWORD_MIN_LENGTH,
-            message: i18next.t('form:min-length', { minLength: PASSWORD_MIN_LENGTH }),
+            message: i18n.t('form:min-length', { minLength: PASSWORD_MIN_LENGTH }),
         },
-        { pattern: strongRegex, message: i18next.t('form:Password validation') },
+        { pattern: strongRegex, message: i18n.t('form:Password validation') },
     ],
     rePassword: [
-        { required: true, message: i18next.t('form:Remember to rewrite password') },
+        { required: true, message: i18n.t('form:Remember to rewrite password') },
         // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
         ({ getFieldValue }) => ({
             validator(_, value) {
                 if (!value || getFieldValue('password') === value) {
                     return Promise.resolve()
                 }
-                return Promise.reject(new Error(i18next.t('form:Must be equal', { field: 'password' })))
+                return Promise.reject(new Error(i18n.t('form:Must be equal', { field: 'password' })))
             },
         }),
     ],
     email: [
-        { required: true, message: i18next.t('form:Remember email') },
-        { type: 'email', message: i18next.t('form:Email is not valid') },
+        { required: true, message: i18n.t('form:Remember email') },
+        { type: 'email', message: i18n.t('form:Email is not valid') },
     ],
-    lastName: [{ required: true, message: i18next.t('form:Remember last name') }],
-    firstName: [{ required: true, message: i18next.t('form:Remember first name') }],
+    lastName: [{ required: true, message: i18n.t('form:Remember last name') }],
+    firstName: [{ required: true, message: i18n.t('form:Remember first name') }],
 }
 
 interface IFormRules {
