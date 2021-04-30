5d6b234065abf54a6c8e438862309ad5fec3e8dd
Sander Hurlen <sander.hurlen@icloud.com>
2021-04-30 20:32:55 +0200

    add useMessage hook for uniformly handle messages

---
+++ src/core/hooks/useMessage.tsx
@@ -0,0 +1,57 @@
+import { message } from 'antd'
+import { ArgsProps, MessageType } from 'antd/lib/message'
+import { ReactNode } from 'react'
+
+type Message = ({
+    content,
+    duration,
+    onClose,
+    key,
+}: {
+    content: ReactNode
+    duration?: number
+    onClose?: () => void
+    key?: string | number
+}) => MessageType
+
+/**
+ * use the message to display messages to user, loading state or other globally available state
+ * @returns a message displayed on the screen
+ */
+export default function useMessage(): {
+    info: Message
+    success: Message
+    error: Message
+    loading: Message
+    warning: Message
+    // eslint-disable-next-line indent
+} {
+    const DURATION = 4
+    const config: ArgsProps = {
+        content: '',
+        duration: DURATION,
+        type: 'info',
+        className: 'anovote-message',
+    }
+    const setDuration = (duration: number | undefined): number => (duration ? duration : DURATION)
+
+    const info: Message = ({ content, duration, onClose, key }) => {
+        return message.open({ ...config, content, type: 'info', onClose, duration: setDuration(duration), key })
+    }
+    const success: Message = ({ content, duration, onClose, key }) => {
+        return message.open({ ...config, content, type: 'success', onClose, duration: setDuration(duration), key })
+    }
+    const error: Message = ({ content, duration, onClose, key }) => {
+        return message.open({ ...config, content, type: 'error', onClose, duration: setDuration(duration), key })
+    }
+
+    const warning: Message = ({ content, duration, onClose, key }) => {
+        return message.open({ ...config, content, type: 'warning', onClose, duration: setDuration(duration), key })
+    }
+
+    const loading: Message = ({ content, duration, onClose, key }) => {
+        return message.open({ ...config, content, type: 'loading', onClose, duration: setDuration(duration), key })
+    }
+
+    return { info, success, error, loading, warning }
+}
