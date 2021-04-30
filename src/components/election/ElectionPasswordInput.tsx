829ac7bc75726d514ef8fa97c8ab2bcd6c2e26a6
Christoffer A Traeen <christoffer.andersen@live.com>
2021-04-30 11:30:58 +0200

    Add localization to password and remove class

--- src/components/election/ElectionPasswordInput.tsx
+++ src/components/election/ElectionPasswordInput.tsx
@@ -6,13 +6,12 @@ export default function ElectionPasswordInput(): React.ReactElement {
     const [t] = useTranslation(['form', 'common'])
     return (
         <Form.Item
-            className="password-input"
             name="password"
             label={t('common:Password')}
-            rules={[{ required: false, message: 'form:Please provide a password' }]}
+            rules={[{ required: false, message: t('form:Please provide a password') }]}
             tooltip={t('election:Add a password as a verification for your election')}
         >
-            <Input.Password placeholder="Password" />
+            <Input.Password placeholder={t('common:Password')} />
         </Form.Item>
     )
 }
