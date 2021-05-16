d85554d543b4f6c8f60d88bee0776e87d35585a4
Sander Hurlen <sander.hurlen@icloud.com>
2021-05-16 14:47:18 +0200

    clean up voter header

--- src/components/voterContentInfo/VoterContentInfo.tsx
+++ src/components/voterContentInfo/VoterContentInfo.tsx
@@ -1,10 +1,11 @@
+import Title from 'antd/lib/typography/Title'
 import React, { ReactElement } from 'react'
 
 export default function VoterContentInfo({ context, title }: { title: string; context?: string }): ReactElement {
     return (
         <div className="voter-election-top">
             {!!context && <span className="text-label">{context}</span>}
-            <h2 className="text-medium">{title}</h2>
+            <Title level={1}>{title}</Title>
         </div>
     )
 }
